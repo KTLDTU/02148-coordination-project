698518a2f21bb729edfb1c13b62b65d2cc7a36c9
KTLDTU <72263629+KTLDTU@users.noreply.github.com>
2023-01-10 20:13:22 +0100

    bit of refactor and add nested listener class

--- src/main/java/application/Room.java
+++ src/main/java/application/Room.java
@@ -14,64 +14,88 @@ import javafx.util.Callback;
 import org.jspace.ActualField;
 import org.jspace.FormalField;
 import org.jspace.Space;
-
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
 
 public class Room {
 
+    private String chatFileName = "/ChatboxView.fxml";
+    private String roomFileName = "/room-scene-view.fxml";
+
     private Scene roomScene;
-    private ChatBoxViewController chatboxController;
+
+    private FXMLLoader chatboxLoader;
+    private FXMLLoader roomLoader;
     private RoomSceneViewController roomController;
-    Space space;
+    private Space space;
+    private String name;
+    private String uri;
     private int players = 0;
+    private ArrayList<String> playerNames;
 
     public Room(Stage stage, GameApplication application, Space space) {
         this.space = space;
-        // Chatbox resource file name
-        String chatFileName = "/ChatboxView.fxml";
-        String roomFileName = "/room-scene-view.fxml";
+        playerNames = new ArrayList();
         try {
-            FXMLLoader roomLoader = new FXMLLoader(RoomSceneViewController.class.getResource(roomFileName));
-            FXMLLoader chatboxLoader = new FXMLLoader(ChatBoxViewController.class.getResource(chatFileName));
+            uri = (String) space.query(new ActualField("clientUri"), new FormalField(String.class))[1];
+            name = (String) space.get(new ActualField("name"), new FormalField(String.class))[1];
 
-            String uri = (String) space.query(new ActualField("clientUri"), new FormalField(String.class))[1];
-            String name = (String) space.get(new ActualField("name"), new FormalField(String.class))[1];
+            updatePlayerNames(space);
 
-            // Try to update the number of players
-            Object[] numberOfPlayers = space.getp(new ActualField("numberOfPlayer"), new FormalField(Integer.class));
-            if (numberOfPlayers != null) {
-                players = (int) numberOfPlayers[1];
-            }
-            players++;
-            space.put("numberOfPlayer", players);
+            updateNumberOfPlayers(space);
 
-            populateChatBoxConstructor(chatboxLoader, uri, name);
+            roomLoader = new FXMLLoader(RoomSceneViewController.class.getResource(roomFileName));
+            chatboxLoader = new FXMLLoader(ChatBoxViewController.class.getResource(chatFileName));
 
-            setupRoomLayout(stage, application, roomLoader, chatboxLoader);
+            populateChatBoxConstructor(uri, players, name);
+
+            setupRoomLayout(stage, application);
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
         stage.setScene(roomScene);
+
     }
 
-    private void setupRoomLayout(Stage stage, GameApplication application, FXMLLoader roomLoader, FXMLLoader chatboxLoader) throws IOException {
+    private void updateNumberOfPlayers(Space space) throws InterruptedException {
+        Object[] numberOfPlayers = space.getp(new ActualField("numberOfPlayer"), new FormalField(Integer.class));
+        if (numberOfPlayers != null) {
+            players = (int) numberOfPlayers[1];
+        }
+        players++;
+        space.put("numberOfPlayer", players);
+    }
+
+    private void updatePlayerNames(Space space) throws InterruptedException {
+        Object[] playerNameLists = space.getp(new ActualField("playerNameList"), new FormalField(ArrayList.class));
+        if (playerNameLists != null) {
+            playerNames = (ArrayList<String>) playerNameLists[1];
+        }
+        playerNames.add(name);
+        space.put("playerNameList", playerNames);
+    }
+
+
+    private void setupRoomLayout(Stage stage, GameApplication application) throws IOException {
         BorderPane chatbox = chatboxLoader.load();
         BorderPane roomLayout = roomLoader.load();
+
+        roomController = roomLoader.getController();
 
         Button lobbyButton = (Button) roomLayout.lookup("#lobbyButton");
         Button startGameButton = (Button) roomLayout.lookup("#startGameButton");
         lobbyButton.setOnAction(e -> stage.setScene(application.lobbyScene));
         startGameButton.setOnAction(e -> application.launchGame(stage));
 
-
         chatbox.setMargin(chatbox, new Insets(0, 2, 0, 0));
         roomLayout.setRight(chatbox);
         roomScene = new Scene(roomLayout, application.WINDOW_WIDTH, application.WINDOW_HEIGHT);
+
+        // After we've set up the scene we start the listener thread to update the ListView when newp players join
+        new Thread(new RoomListener(space, roomController)).start();
     }
 
-    private void populateChatBoxConstructor(FXMLLoader chatboxLoader, String uri, String name) {
+    private void populateChatBoxConstructor(String uri, int players, String name) {
         ArrayList arrayData = new ArrayList();
         arrayData.add(uri);
         arrayData.add(players);
@@ -92,5 +116,32 @@ public class Room {
                     }
             }
         });
+    }
+}
+
+class RoomListener implements Runnable {
+    Space space;
+    RoomSceneViewController roomController;
+    ArrayList<String> playerNames = new ArrayList<>();
+
+    public RoomListener(Space space, RoomSceneViewController roomController) {
+        this.space = space;
+        this.roomController = roomController;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                ArrayList<String> newPlayerNames = (ArrayList<String>) space.query(new ActualField("playerNameList"), new FormalField(ArrayList.class))[1];
+                // Update list of player names if the two lists are different
+                if (!playerNames.equals(newPlayerNames)) {
+                    playerNames = newPlayerNames;
+                    roomController.updatePlayerList(newPlayerNames);
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
