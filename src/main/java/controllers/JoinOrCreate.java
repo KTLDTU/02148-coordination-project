c9db84900777fe23940960e19dcd63f7274cf349
Souar-Afrin <siwar.95.95@gmail.com>
2023-01-12 15:30:10 +0100

    create createOrJoin fxml and controller

--- src/main/java/controllers/JoinOrCreate.java
+++ src/main/java/controllers/JoinOrCreate.java
@@ -1,4 +1,24 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
 public class JoinOrCreate {
+
+    @FXML
+    public Button btnJoin;
+    @FXML
+    public Button btnCreate;
+    @FXML
+    public TextField textField;
+
+    @FXML
+    void initialize() {
+        assert btnCreate != null : "fx:id=\"btnCreate\" was not injected: check your FXML file 'joinOrCreate.fxml'.";
+        assert btnJoin != null : "fx:id=\"btnJoin\" was not injected: check your FXML file 'joinOrCreate.fxml'.";
+        assert textField != null : "fx:id=\"textField\" was not injected: check your FXML file 'joinOrCreate.fxml'.";
+    }
+
 }
