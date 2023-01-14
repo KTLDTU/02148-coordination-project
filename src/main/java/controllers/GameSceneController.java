a5d3e710c44d97ba03a7dab46aaa3c56cfda0c7b
KTLDTU <72263629+KTLDTU@users.noreply.github.com>
2023-01-14 12:22:17 +0100

    Added layout to hold player names and initialize score to 0

--- src/main/java/controllers/GameSceneController.java
+++ src/main/java/controllers/GameSceneController.java
@@ -1,10 +1,15 @@
 package controllers;
 
+import application.GameApplication;
 import application.Grid;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+
+import java.util.ArrayList;
 
 public class GameSceneController {
 
@@ -15,12 +20,12 @@ public class GameSceneController {
     private BorderPane scene;
 
     @FXML
-    private Text playerScores;
+    private HBox playerScoreContainer;
 
     @FXML
     void initialize() {
         assert gamePane != null : "fx:id=\"game\" was not injected: check your FXML file 'game-scene-view.fxml'.";
-        assert playerScores != null : "fx:id=\"player_scores\" was not injected: check your FXML file 'game-scene-view.fxml'.";
+        assert playerScoreContainer != null : "fx:id=\"player_scores\" was not injected: check your FXML file 'game-scene-view.fxml'.";
         assert scene != null : "fx:id=\"scene\" was not injected: check your FXML file 'game-scene-view.fxml'.";
     }
 
@@ -30,5 +35,18 @@ public class GameSceneController {
 
         for (var wall : grid.verticalWalls)
             gamePane.getChildren().add(wall);
+    }
+
+    public void initializePlayerNames(ArrayList<String> playerNames) {
+        for (String name : playerNames) {
+            HBox nameContainer = new HBox(5);
+            Text nameText = new Text(name + ":");
+            Text scoreText = new Text("0");
+
+            nameContainer.setAlignment(Pos.CENTER);
+            nameContainer.setPrefWidth(GameApplication.WINDOW_WIDTH/playerNames.size());
+            nameContainer.getChildren().addAll(nameText, scoreText);
+            playerScoreContainer.getChildren().add(nameContainer);
+        }
     }
 }
