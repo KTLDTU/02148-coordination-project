4d8125c8290773b5c3ef0743b9a1b03b1951e7d7
KTLDTU <72263629+KTLDTU@users.noreply.github.com>
2023-01-16 12:18:45 +0100

    move RoomListener

--- src/main/java/application/Room.java
+++ src/main/java/application/Room.java
@@ -3,7 +3,6 @@ package application;
 import controllers.ChatBoxViewController;
 import controllers.RoomSceneViewController;
 import datatypes.ArrayListInt;
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +11,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.util.Callback;
+import listeners.RoomListener;
 import org.jspace.ActualField;
 import org.jspace.FormalField;
 import org.jspace.Space;
@@ -126,32 +126,3 @@ public class Room {
     }
 }
 
-class RoomListener implements Runnable {
-    Space space;
-    RoomSceneViewController roomController;
-    ArrayList<String> playerNames = new ArrayList<>();
-
-    public RoomListener(Space space, RoomSceneViewController roomController) {
-        this.space = space;
-        this.roomController = roomController;
-    }
-
-    @Override
-    public void run() {
-        while (true) {
-            try {
-                ArrayList<String> newPlayerNames = (ArrayList<String>) space.query(new ActualField("playerNameList"), new FormalField(ArrayList.class))[1];
-
-                // Update list of player names if the two lists are different
-                if (!playerNames.equals(newPlayerNames)) {
-                    playerNames = newPlayerNames;
-                    Platform.runLater(() -> {
-                        roomController.updatePlayerList(newPlayerNames);
-                    });
-                }
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-}
