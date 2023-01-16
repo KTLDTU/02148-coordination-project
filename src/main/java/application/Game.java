46e481d044e04790a76716794c7bb60cc5189fad
KTLDTU <72263629+KTLDTU@users.noreply.github.com>
2023-01-16 12:16:55 +0100

    move ShotListener

--- src/main/java/application/Game.java
+++ src/main/java/application/Game.java
@@ -1,6 +1,5 @@
 package application;
 
-import broadcasters.KillBroadcaster;
 import broadcasters.PlayerPositionBroadcaster;
 import controllers.GameSceneController;
 import controllers.MovementController;
@@ -15,6 +14,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import listeners.InputListener;
+import listeners.ShotListener;
 import org.jspace.ActualField;
 import org.jspace.FormalField;
 import org.jspace.Space;
@@ -174,40 +174,6 @@ class MovementListener implements Runnable {
                         tractor.setLayoutX(tractorX);
                         tractor.setLayoutY(tractorY);
                         tractor.setRotate(tractorRot);
-                    }
-                });
-            }
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-}
-
-class ShotListener implements Runnable {
-    private Game game;
-
-    public ShotListener(Game game) {
-        this.game = game;
-    }
-
-    @Override
-    public void run() {
-        try {
-            while (true) {
-                Object[] obj = game.gameSpace.get(new ActualField("new shot"), new FormalField(Integer.class), new ActualField(game.MY_PLAYER_ID), new FormalField(Integer.class), new FormalField(Double.class), new FormalField(Double.class), new FormalField(Double.class));
-                int playerID = (int) obj[1];
-                int shotID = (int) obj[3];
-                double shotX = (double) obj[4];
-                double shotY = (double) obj[5];
-                double shotRot = (double) obj[6];
-
-                Platform.runLater(() -> {
-                    Shot shot = game.shotController.shoot(shotX, shotY, shotRot, playerID, shotID);
-                    game.shots.put(shotID, shot);
-
-                    // if a player shoots directly into a wall, they die immediately
-                    if (GameApplication.isHost && game.grid.isWallCollision(shot)) {
-                        new Thread(new KillBroadcaster(game, playerID, shotID)).start();
                     }
                 });
             }
